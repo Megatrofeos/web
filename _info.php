23830a69fe6e1fa1b6227ba636971bbac0f0d5e9
MEgaTrofeos <info@megatrofeos.com>
2019-07-15 09:05:22 +0200

    asdasdasd
    asdasdasd

---
+++ _info.php
@@ -0,0 +1,36 @@
+
+29.09.2018 00:28 : theme http://aesthetic-bd.com/themeforest/becool/app-landing.html
+29.09.2018 00:28 : http://preview.themeforest.net/item/anfang-agency-startup-and-saas-template/full_screen_preview/22558511?_ga=2.58951087.300927814.1538172407-674456251.1537909779
+29.09.2018 00:33 : MENU: iDomotic | Tienda online | Instalación domótica | Contacto
+29.09.2018 00:33 : MENU FOOTER: Montajes | Dispositivos | Compatibilidad | Domótica
+
+http://www.esploradores.com/conexion-del-procesador-esp8266-como-cliente-de-un-servidor-web/
+http://kio4.com/arduino/57modulowifi_2.htm
+
+
+
+
+--- PROGRAMA
+https://stackoverflow.com/questions/48702611/mqtt-connection-with-ionic-3
+
+
+
+tests
+
+--- APP
+http://megebaek.dk/2018/11/07/setup-mqtt-on-synology-docker/
+https://github.com/sclausen/ngx-mqtt/issues/75
+https://github.com/sclausen/ngx-mqtt/blob/master/tests/mqtt.service.spec.ts
+https://www.npmjs.com/package/angular2-mqtt
+https://sclausen.github.io/ngx-mqtt/#usage
+https://stackoverflow.com/questions/48702611/mqtt-connection-with-ionic-3
+
+
+
+
+99
+
+asdasd
+asdasdasdas
+asdasdasdasdasd
+asdasdasdasdasdasdasd
